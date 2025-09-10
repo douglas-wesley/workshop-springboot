e0515d8659400bee3cd3166313074dc2bf6545d1
douglas-wesley <douglas0407@gmail.com>
2025-09-10 17:56:41 -0300

    feat: Adicionando a entidade 'Category'

--- src/main/java/br/com/course/config/TestConfig.java
+++ src/main/java/br/com/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package br.com.course.config;
 
+import br.com.course.entities.Category;
 import br.com.course.entities.Order;
 import br.com.course.entities.User;
 import br.com.course.enums.OrderStatus;
+import br.com.course.repositories.CategoryRepository;
 import br.com.course.repositories.OrderRepository;
 import br.com.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Douglas", "douglas@gmail.com", "819743245", "1234");
@@ -31,10 +36,14 @@ public class TestConfig implements CommandLineRunner {
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT);
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
-
-
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
     }
 
 
---
+++ src/main/java/br/com/course/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package br.com.course.repositories;
+
+import br.com.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
